887d30696312ddd9e4c1bbabbc566b124cf72c19
Pavan-M-S <pavan310512@gmail.com>
2026-08-01 21:39:46 +0530

    typp

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 var AppSettings = {
     // 1. Settings for the search-based websites WITH your movie query
     specificUrls: {
-        embed: false,
+        embed: true,
         showLinks: false,
         showNames: true
     },
